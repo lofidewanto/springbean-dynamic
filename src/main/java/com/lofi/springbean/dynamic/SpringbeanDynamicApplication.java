19d1d9d4fac62067730761df6190b1f6c3d3d5e9
lofidewanto <lofidewanto@gmail.com>
2018-11-11 15:18:50 +0100

    first import

---
+++ src/main/java/com/lofi/springbean/dynamic/SpringbeanDynamicApplication.java
@@ -0,0 +1,12 @@
+package com.lofi.springbean.dynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbeanDynamicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbeanDynamicApplication.class, args);
+	}
+}

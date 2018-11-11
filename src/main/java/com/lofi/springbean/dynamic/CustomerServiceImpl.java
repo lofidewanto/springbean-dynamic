12c7386b7736c5864ee790ac0e764051b5235af4
lofidewanto <lofidewanto@gmail.com>
2018-11-11 21:12:49 +0100

    format

--- src/main/java/com/lofi/springbean/dynamic/CustomerServiceImpl.java
+++ src/main/java/com/lofi/springbean/dynamic/CustomerServiceImpl.java
@@ -10,22 +10,22 @@ import org.springframework.stereotype.Service;
 public class CustomerServiceImpl {
 
 	private Customer dynamicCustomerWithAspect;
-	
+
 	// We inject the customer implementations into a Map
-    @Autowired
-    private Map<String, Customer> dynamicCustomerWithMap;
-    
-    // This comes from the property file as a key for the Map
-    @Value("${dynamic.object.name}")
+	@Autowired
+	private Map<String, Customer> dynamicCustomerWithMap;
+
+	// This comes from the property file as a key for the Map
+	@Value("${dynamic.object.name}")
 	private String object;
 
 	@InjectDynamicObject
 	public Customer getDynamicCustomerWithAspect() {
 		return this.dynamicCustomerWithAspect;
 	}
-    
-    public Customer getDynamicCustomerWithMap() {
-        return this.dynamicCustomerWithMap.get(object);
-    }
+
+	public Customer getDynamicCustomerWithMap() {
+		return this.dynamicCustomerWithMap.get(object);
+	}
 
 }
